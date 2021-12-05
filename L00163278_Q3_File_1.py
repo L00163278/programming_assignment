0167af690cda4595b241d3146c9807bb92337329
L00163278 <91317208+L00163278@users.noreply.github.com>
2021-12-05 20:42:57 +0000

    Add files via upload

---
+++ L00163278_Q3_File_1.py
@@ -0,0 +1,49 @@
+"""
+# -----------------------------
+# File : L00163278_Q3_File_1.py
+# Created :  27/11/2021 20:25
+# Author : Rohit Mishra
+# Version : v1.0.0
+# Licensing : (C) 2021 Rohit Mishra, LYIT
+#              Available under GNU Public License (GPL)
+# Description : Connection test to a VM using SSH port
+# -----------------------------
+"""
+
+import paramiko
+import time
+import re
+
+
+# Open SSH connection to the device
+# first install ssh-server on the VM
+#           sudo apt install openssh-server openssh-client
+#
+def ssh_connection(ip):
+    """
+    """
+    try:
+        username = "l00163278"
+        password = "Qwerty123$"
+
+        print("Establishing a connection...")
+        session = paramiko.SSHClient()
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        session.connect(ip.rstrip("\n"), username=username, password=password)
+        connection = session.invoke_shell()
+        connection.send("ls -al > longList.txt\n")  # unix command to list directory contents and save to file
+        time.sleep(1)
+
+        vm_output = connection.recv(65535)
+        if re.search(b"% Invalid input", vm_output):
+            print("There was at least one IOS syntax error on device {}".format(ip))
+        else:
+            print("Commands successfully executed on {}".format(ip))
+        session.close()
+    except paramiko.AuthenticationException as err:
+        print(err)
+        print("Authentication Error")
+
+
+if __name__ == "__main__":
+    ssh_connection("192.168.150.128")
---
+++ L00163278_Q4_File_2.py
@@ -0,0 +1,76 @@
+"""
+# -----------------------------
+# File : L00163278_Q4_File_2.py
+# Created :  27/11/2021 21:22
+# Author : Rohit Mishra
+# Version : v1.0.0
+# Licensing : (C) 2021 Rohit Mishra, LYIT
+#              Available under GNU Public License (GPL)
+# Description : Display which ports are open in the Ubuntu VM
+# -----------------------------
+"""
+
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+
+def port_scan():
+    """
+    """
+    # Clear the screen  #use clear if running in  *nix
+    subprocess.call("cls", shell=True)
+
+    # Ask for input
+    remoteServer = input("Enter a remote host to scan: ")
+    remoteServerIP = socket.gethostbyname(remoteServer)
+
+    # Print a nice banner with information on which host we are about to scan
+    print("-" * 60)
+    print("Please wait, scanning remote host", remoteServerIP)
+    print("-" * 60)
+
+    # Check what time the scan started
+    t1 = datetime.now()
+
+    # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+    # We also put in some error handling for catching errors
+
+    try:
+        # try 1, 1025 if you have time
+        for port in range(1, 81):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                name = ''  # initial value.
+                if port == 80:
+                    name = 'HTTP'
+                elif port == 22:
+                    name = 'SSH'
+                print("Port {}: 	 Open     {}".format(port, name))
+            sock.close()
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C")
+        sys.exit()
+
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    except socket.error:
+        print("Couldn't connect to server")
+        sys.exit()
+
+    # Checking the time again
+    t2 = datetime.now()
+
+    # Calculates the difference of time, to see how long it took to run the script
+    total = t2 - t1
+
+    # Printing the information to screen
+    print('Scanning Completed in: ', total)
+
+
+if __name__ == "__main__":
+    port_scan()
